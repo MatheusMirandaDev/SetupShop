246f2985cf8fc0f71c059b4da18aa654cab7aa72
Matheus Miranda <matheusmiranda.batista@gmail.com>
2026-07-24 00:04:27 -0300

    test: add unit test for product creation use case

---
+++ src/test/java/br/com/setupshop/product/application/usecase/create/CreateProductUseCaseTest.java
@@ -0,0 +1,43 @@
+package br.com.setupshop.product.application.usecase.create;
+
+import br.com.setupshop.product.domain.model.Product;
+import br.com.setupshop.product.domain.repository.ProductRepository;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+class CreateProductUseCaseTest {
+
+    @Test
+    void shouldCreateAndSaveProduct() {
+        // Given
+        ProductRepository productRepository = mock(ProductRepository.class);
+        CreateProductUseCase productUseCase = new CreateProductUseCase(productRepository);
+
+        CreateProductCommand productCommand = new CreateProductCommand(
+                "Teclado AULA F75",
+                "Teclado Mecanico para computador",
+                new BigDecimal("300.00")
+
+        );
+        when(productRepository.save(any(Product.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+        // When
+        Product productResult = productUseCase.execute(productCommand);
+
+        // Then
+        assertEquals("Teclado AULA F75", productResult.getName());
+
+        assertEquals("Teclado Mecanico para computador", productResult.getDescription());
+        assertEquals(new BigDecimal("300.00"), productResult.getPrice());
+        assertTrue(productResult.isActive());
+
+        verify(productRepository, times(1)).save(any(Product.class));
+
+    }
+}
