ca337dc0a29fea3c0e4a86405c0840c69eb7fea3
Matheus Miranda <matheusmiranda.batista@gmail.com>
2026-07-27 19:39:08 -0300

    feat: add product creation REST endpoint

---
+++ src/main/java/br/com/setupshop/SetupShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.setupshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetupShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetupShopApplication.class, args);
+    }
+
+}
--- src/main/java/br/com/setupshop/product/infrastructure/web/ProductController.java
+++ src/main/java/br/com/setupshop/product/infrastructure/web/ProductController.java
@@ -1,7 +1,50 @@
 package br.com.setupshop.product.infrastructure.web;
 
-import org.springframework.stereotype.Controller;
+import br.com.setupshop.product.application.usecase.create.CreateProductCommand;
+import br.com.setupshop.product.application.usecase.create.CreateProductUseCase;
+import br.com.setupshop.product.domain.model.Product;
+import br.com.setupshop.product.infrastructure.web.dto.CreateProductRequest;
+import br.com.setupshop.product.infrastructure.web.dto.ProductResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+
+    private final CreateProductUseCase productUseCase;
+
+    public ProductController(CreateProductUseCase productUseCase) {
+        this.productUseCase = productUseCase;
+    }
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(@Valid @RequestBody CreateProductRequest request) {
+
+        CreateProductCommand command = new CreateProductCommand(
+                request.name(),
+                request.description(),
+                request.price()
+        );
+
+        Product product = productUseCase.execute(command);
+
+        ProductResponse response = new ProductResponse(
+                product.getId(),
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.isActive()
+        );
+
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
