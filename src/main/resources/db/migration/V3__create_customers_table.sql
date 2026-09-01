145489ec09e91cb7d22abac628807e109d40f040
Matheus Miranda <matheusmiranda.batista@gmail.com>
2026-09-01 06:00:52 -0300

    fix: correct customer migration syntax

--- src/main/resources/db/migration/V3__create_customers_table.sql
+++ src/main/resources/db/migration/V3__create_customers_table.sql
@@ -6,16 +6,13 @@ CREATE TABLE customers
     phone      VARCHAR(11)              NOT NULL,
     active     BOOLEAN                  NOT NULL DEFAULT TRUE,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-        CONSTRAINT ck_customers_name_not_blank
-            CHECK (BTRIM(name) <> ''),
+    CONSTRAINT ck_customers_name_not_blank
+        CHECK (BTRIM(name) <> ''),
 
     CONSTRAINT ck_customers_email_not_blank
         CHECK (BTRIM(email) <> ''),
-
-    CONSTRAINT ck_customers_phone_not_blank
-        CHECK (BTRIM(phone) <> ''),
 
     CONSTRAINT ck_customers_phone_exactly_eleven_digits
         CHECK (phone ~ '^[0-9]{11}$')
